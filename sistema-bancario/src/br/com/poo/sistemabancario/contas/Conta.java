b731baa18d4a0e338860e5ab343a135fac5e8ac7
grazifalk <grazifalk@gmail.com>
2022-09-03 12:56:23 -0300

    Atualização com função transferir

--- sistema-bancario/src/br/com/poo/sistemabancario/contas/Conta.java
+++ sistema-bancario/src/br/com/poo/sistemabancario/contas/Conta.java
@@ -1,51 +1,77 @@
 package br.com.poo.sistemabancario.contas;
 
 public class Conta {
+
+	// declaracao de variaveis
+	public int numero;
+	public String titular;
+	public double saldo;
 	
-		// declaracao de variaveis
-		public int numero;
-		public String titular;
-		public double saldo;
-		
-		//acoes da conta
-		
-		// metodo de saque sem tratamento
-	/*	public void sacar(double valor) {
+	// acoes da conta
+
+	// metodo de saque sem tratamento
+	/*
+	 * public void sacar(double valor) { double novoSaldo = this.saldo - valor;
+	 * this.saldo = novoSaldo; }
+	 */
+
+	// metodo de saque com tratamento parcial
+	public boolean sacar(double valor) {
+		if (this.saldo < valor) {
+			System.out.println("Saldo insuficiente!");
+			return false;
+		} else {
 			double novoSaldo = this.saldo - valor;
 			this.saldo = novoSaldo;
-		}*/
-		
-		//metodo de saque com tratamento parcial
-		public boolean sacar(double valor) {
-			if (this.saldo < valor) {
-				System.out.println("Saldo insuficiente!");
-				return false;
-			} else {
-				double novoSaldo = this.saldo - valor;
-				this.saldo = novoSaldo;
-				
-				return true;
-			}
-		}
 
-		//metodo para efetuar um deposito sem tratamento
-	/*	public void depositar(double valor) {
-			//double novoSaldo = this.saldo + valor;
-			//this.saldo = novoSaldo;    
+			return true;
+		}
+	}
+
+	// metodo para efetuar um deposito sem tratamento
+	/*
+	 * public void depositar(double valor) { //double novoSaldo = this.saldo +
+	 * valor; //this.saldo = novoSaldo; this.saldo += valor; }
+	 */
+
+	// metodo para efetuar um deposito com tratamento parcial
+
+	public boolean depositar(double valor) {
+		if (valor < 0) {
+			System.out.println("Valor inválido!");
+			return false;
+		} else {
 			this.saldo += valor;
+			return true;
 		}
-	*/
+	}
 
-		//metodo para efetuar um deposito com tratamento parcial
-		
-		public boolean depositar(double valor) {
-			if(valor < 0) {
-				System.out.println("Valor inválido!");
-				return false;
-			} else {
-				this.saldo += valor;
-				return true;
-			}
+	// transferencia entre contas
+
+//	public boolean tranferir(double valor) {
+//		if (valor < 0) {
+//			System.out.println("Valor inválido!");
+//			return false;
+//		}
+//
+//		else if (valor > saldo) {
+//			System.out.println("Saldo insuficiente!");
+//			return false;
+//		}
+//		else {
+//			this.saldo -= valor;
+//			return true;
+//		}
+//	}
+
+	public boolean transferir(Conta x, double valor) {
+		if (valor <= saldo && valor > 0) {
+			this.sacar(valor);
+			x.depositar(valor);
+			return true;
+		} else {
+			return false;
 		}
-		
+	}
+
 }
--- sistema-bancario/src/br/com/poo/sistemabancario/principal/Principal.java
+++ sistema-bancario/src/br/com/poo/sistemabancario/principal/Principal.java
@@ -10,11 +10,23 @@ public class Principal {
 		minhaConta.titular = "Pedro";
 		minhaConta.saldo = 1000.0;
 		minhaConta.numero = 123;
-		minhaConta.sacar(505.0);
-		minhaConta.depositar(-20.0);
+		minhaConta.sacar(0.0);
+		minhaConta.depositar(0.0);
+			
+		Conta minhaConta2 = new Conta();
 		
+		minhaConta2.titular = "Maria";
+		minhaConta2.saldo = 2000.0;
+		minhaConta2.numero = 124;
+		minhaConta2.sacar(0.0);
+		minhaConta2.depositar(0.0);
+		
+		if(minhaConta.transferir(minhaConta2,-200)) {
+            System.out.println("Transferencia ocorreu com sucesso");
+        } else { System.out.println("Saldo insuficiente para transação!");}
 		
 		System.out.println("Saldo atual: " + minhaConta.saldo + "\nNome do titular: " + minhaConta.titular + "\nNúmero da Conta:" + minhaConta.numero);
+		System.out.println("\nSaldo atual: " + minhaConta2.saldo + "\nNome do titular: " + minhaConta2.titular + "\nNúmero da Conta:" + minhaConta2.numero);
 
 	}
 
